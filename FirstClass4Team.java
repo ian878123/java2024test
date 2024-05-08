8dcc365ff7d5258dd2fc6d8e265eb9c8b6fc93d8
linsitin <zaq0809449@gmail.com>
2024-05-08 16:00:20 +0800

    FIX: NEW LINE2

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -3,5 +3,6 @@ public class FirstClass4Team {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01157147\"\n姓名：\"蕭英祐\"");
         System.out.println("\"學號：\"01157108\"\n姓名：\"張俊淇\"");
+        System.out.println("\"學號：\"01157103\"\n姓名：\"林思霆\"");
     }
 }
