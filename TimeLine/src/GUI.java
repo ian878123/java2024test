bd52b43e2f885c594ce1d020f791dcea7bb8a346
findketsuro <kikijimmg@gmail.com>
2024-05-24 13:59:29 +0800

    Enables the GUI to be split into two parts

--- TimeLine/src/GUI.java
+++ TimeLine/src/GUI.java
@@ -3,7 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class GUI extends JFrame implements ItemListener, ActionListener {
     Label labels=new Label("labels");
@@ -11,8 +11,21 @@ public class GUI extends JFrame implements ItemListener, ActionListener {
         super("Time Line");
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setSize(screenSize.width,screenSize.height);
+        GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().setFullScreenWindow(this);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
+
+        JPanel leftPanel = new JPanel();
+        leftPanel.setBackground(Color.LIGHT_GRAY);
+        leftPanel.add(new JLabel("Time Line"));
+
+        JPanel rightPanel = new JPanel();
+        rightPanel.setBackground(Color.PINK);
+        rightPanel.add(new JLabel("Control Panel"));
+
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftPanel, rightPanel);
+        splitPane.setDividerLocation((int) (screenSize.width * 0.8));
+        splitPane.setEnabled(false);
+        add(splitPane);
     }
 
     public void itemStateChanged(ItemEvent e) {
