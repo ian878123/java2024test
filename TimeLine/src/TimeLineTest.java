931fc1f2f40976ed1d84412d6576afd3479d95fd
findketsuro <kikijimmg@gmail.com>
2024-05-25 14:56:23 +0800

    main class renamed

---
+++ TimeLine/src/TimeLineTest.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class TimeLineTest {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            GUI frame = new GUI();
+            frame.setVisible(true);
+        });
+    }
+}
